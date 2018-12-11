e2bfe7e0427cdd069dfe53f0536e6080b55a0bcc
Ganesh Viswanathan <dev@genotrance.com>
2018-12-10 22:25:31 -0600

    Fix full test crash on 0.19.0+

--- tests/tscinfull.nim
+++ tests/tscinfull.nim
@@ -1,33 +1,72 @@
 const
-  WS_VISIBLE* = 0x10000000
-  WS_TABSTOP* = 0x00010000
-  WS_CLIPCHILDREN* = 0x02000000
-
+  WS_OVERLAPPED* = 0x00000000
+  WS_CAPTION* = 0x00C00000
+  WS_SYSMENU* = 0x00080000
+  WS_THICKFRAME* = 0x00040000
+  WS_MINIMIZEBOX* = 0x00020000
+  WS_MAXIMIZEBOX* = 0x00010000
+  WS_OVERLAPPEDWINDOW* = WS_OVERLAPPED or WS_CAPTION or WS_SYSMENU or WS_THICKFRAME or WS_MINIMIZEBOX or WS_MAXIMIZEBOX
+  WM_QUIT* = 0x0012
   SW_SHOW* = 5
 
 type
-  MSG* = object
+  HANDLE* = int
+  HWND* = HANDLE
+  HINSTANCE* = HANDLE
+  HMENU* = HANDLE
+  LPVOID* = pointer
+  WINBOOL* = int32
+  HMODULE* = HINSTANCE
+  DWORD* = int32
+  uint_PTR* = int64
+  LONG_PTR* = int64
+  WPARAM* = uint_PTR
+  LPARAM* = LONG_PTR
+  WCHAR* = uint16
+  LPCWSTR* = ptr WCHAR
+  LRESULT* = LONG_PTR
+  LPCSTR* = cstring
 
-proc GetModuleHandleW*(lpModuleName: WideCString): pointer {.stdcall, dynlib: "kernel32", importc.}
+  POINT* {.pure.} = object
+    x*: clong
+    y*: clong
 
-proc GetLastError*(): int {.stdcall, dynlib: "kernel32", importc.}
+  MSG* {.pure.} = object
+    hwnd*: HWND
+    message*: uint
+    wParam*: WPARAM
+    lParam*: LPARAM
+    time*: DWORD
+    pt*: POINT
 
-proc CreateWindowExW*(dwExStyle: int32, lpClassName: WideCString,
-  lpWindowName: WideCString, dwStyle: int32, X: int32, Y: int32,
-  nWidth: int32, nHeight: int32, hWndParent: int, hMenu: int,
-  hInstance: pointer, lpParam: pointer): pointer {.stdcall, dynlib: "user32", importc.}
+  LPMSG* = ptr MSG
 
-proc IsWindow*(hWnd: pointer): bool {.stdcall, dynlib: "user32", importc.}
+proc GetModuleHandleW*(lpModuleName: LPCWSTR): HMODULE {.stdcall, dynlib: "kernel32", importc.}
 
-proc ShowWindow*(hWnd: pointer, nCmdShow: int): int {.stdcall, dynlib: "user32", importc.}
+proc GetLastError*(): DWORD {.stdcall, dynlib: "kernel32", importc.}
 
-proc UpdateWindow*(hWnd: pointer): int {.stdcall, dynlib: "user32", importc.}
+proc CreateWindowExW*(dwExStyle: DWORD, lpClassName: LPCWSTR, lpWindowName: LPCWSTR, dwStyle: DWORD,
+  X: int32, Y: int32, nWidth: int32, nHeight: int32, hWndParent: HWND, hMenu: HMENU,
+  hInstance: HINSTANCE, lpParam: LPVOID): HWND {.stdcall, dynlib: "user32", importc.}
 
-proc GetMessageW*(lpMsg: pointer, hWnd: pointer, wMsgFilterMin: uint, wMsgFilterMax: uint): int {.stdcall, dynlib: "user32", importc.}
+proc CreateWindowExA*(dwExStyle: DWORD, lpClassName: LPCSTR, lpWindowName: LPCSTR, dwStyle: DWORD, X: int32, Y: int32, nWidth: int32,
+  nHeight: int32, hWndParent: HWND, hMenu: HMENU, hInstance: HINSTANCE, lpParam: LPVOID): HWND {.stdcall, dynlib: "user32", importc.}
 
-proc TranslateMessage*(lpMsg: pointer): int {.stdcall, dynlib: "user32", importc.}
+proc CreateWindow*(lpClassName: LPCSTR, lpWindowName: LPCSTR, dwStyle: DWORD, x: int32, y: int32, nWidth: int32,
+  nHeight: int32, hWndParent: HWND, hMenu: HMENU, hInstance: HINSTANCE, lpParam: LPVOID): HWND {.inline.} =
+  CreateWindowExA(0, lpClassName, lpWindowName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam)
 
-proc DispatchMessageW*(lpMsg: pointer): pointer {.stdcall, dynlib: "user32", importc.}
+proc IsWindow*(hWnd: HWND): WINBOOL {.stdcall, dynlib: "user32", importc.}
+
+proc ShowWindow*(hWnd: HWND, nCmdShow: int32): WINBOOL {.stdcall, dynlib: "user32", importc.}
+
+proc UpdateWindow*(hWnd: HWND): WINBOOL {.stdcall, dynlib: "user32", importc.}
+
+proc GetMessageW*(lpMsg: LPMSG, hWnd: HWND, wMsgFilterMin: uint, wMsgFilterMax: uint): WINBOOL {.stdcall, dynlib: "user32", importc.}
+
+proc TranslateMessage*(lpMsg: ptr MSG): WINBOOL {.stdcall, dynlib: "user32", importc.}
+
+proc DispatchMessageW*(lpMsg: ptr MSG): LRESULT {.stdcall, dynlib: "user32", importc.}
 
 import nimscintilla/[Scintilla, SciLexer]
 
@@ -37,17 +76,19 @@ if Scintilla_RegisterClasses(nil) == 0:
 
 var
   msg: MSG
-  window = CreateWindowExW(0, newWideCString("Scintilla"), newWideCString(""), WS_VISIBLE or WS_TABSTOP or WS_CLIPCHILDREN, 10, 10, 500, 400, 0, 0, GetModuleHandleW(nil), nil)
+  window = CreateWindow("Scintilla".LPCSTR, "".LPCSTR, WS_OVERLAPPEDWINDOW, 10, 10, 500, 400, 0, 0, GetModuleHandleW(nil), nil)
 
-if not IsWindow(window):
+if IsWindow(window) == 0:
   echo GetLastError()
   quit(1)
 
 echo window.ShowWindow(SW_SHOW)
 echo window.UpdateWindow()
 
-while GetMessageW(addr msg, nil, 0, 0) != 0:
-  echo TranslateMessage(addr msg)
+while GetMessageW(cast[LPMSG](addr msg), 0, 0, 0) != 0:
+  discard TranslateMessage(addr msg)
+  if msg.message == WM_QUIT:
+    break
   discard DispatchMessageW(addr msg)
 
 echo Scintilla_ReleaseResources()
